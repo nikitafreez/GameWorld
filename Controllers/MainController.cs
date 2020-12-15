767eef82be6b2ae8c95b62f9ef5493aa6ea1516a
nikitafreez <nikita2002393@gmail.com>
2020-12-15 16:38:21 +0300

    Recreate all pages in ASP.NET
    Practice Work №2

---
+++ Controllers/MainController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameWorld.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Main() //complete
+        {
+            return View();
+        }
+
+        public IActionResult Catalog() //complete
+        {
+            return View();
+        }
+
+        public IActionResult Cart() //complete
+        {
+            return View();
+        }
+
+        public IActionResult Auth() //complete
+        {
+            return View();
+        }
+
+        public IActionResult About() //complete
+        {
+            return View();
+        }
+
+        public IActionResult MyCabinet() //complete
+        {
+            return View();
+        }
+
+        public IActionResult Recovery() //complete
+        {
+            return View();
+        }
+
+        public IActionResult Reg() //complete
+        {
+            return View();
+        }
+    }
+}
